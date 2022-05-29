34e07661b13d113b1e199c34740aa3585c5b80bb
Manav Misra <manavm@visionwebsoft.com>
2022-05-29 17:21:13 -0500

    test: :white_check_mark:

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,46 @@
-import { add2Nums } from './index';
+import getMostCommonLetters, {
+  removePunc,
+  removeSpaces,
+  tallyLetters,
+} from './index';
 
-it('adds 2 + 2 to equal 3', () => {
-  expect(add2Nums(2, 2)).toBe(4);
+const test = 'Every good boy does fine.';
+
+describe('getHighestLetterFrequency', () => {
+  it('should remove spaces', () => {
+    expect(removeSpaces(test)).toBe('Everygoodboydoesfine.');
+  });
+
+  it('should remove punctuation', () => {
+    expect(removePunc(test)).toBe('Every good boy does fine');
+  });
+
+  it('should tally up the letters', () => {
+    expect(tallyLetters('Every good boy does fine.')).toEqual({
+      e: 4,
+      v: 1,
+      r: 1,
+      y: 2,
+      g: 1,
+      o: 4,
+      d: 2,
+      b: 1,
+      s: 1,
+      f: 1,
+      i: 1,
+      n: 1,
+    });
+  });
+
+  it('should give the highest frequency letter', () => {
+    expect(getMostCommonLetters('aabc')).toEqual(['a']);
+  });
+
+  it('should give the highest frequency letters', () => {
+    expect(getMostCommonLetters(test)).toEqual(['e', 'o']);
+  });
+
+  it('should return an empty array if there are no letters', () => {
+    expect(getMostCommonLetters('')).toEqual([]);
+  });
 });
